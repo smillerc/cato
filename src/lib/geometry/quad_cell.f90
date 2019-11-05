0d00c079f7d1b8f446f22fbff267a583a6571ca7
Miller, Samuel <smil@lle.rochester.edu>
2019-11-05 15:37:54 -0500

    Minor fix

--- src/lib/geometry/quad_cell.f90
+++ src/lib/geometry/quad_cell.f90
@@ -1,34 +1,33 @@
 module mod_quad_cell
   !< Summary: Define the quadrilateral cell/control volume object
 
-  use iso_fortran_env, only : int32, real64
+  use iso_fortran_env, only: int32, real64
 
   implicit none
-  
 
-  !  Numbering convention for a 2D quadrilateral cell
-  !  
-  !                     F3
-  !                     M3
-  !              N4-----o-----N3
-  !              |            |  
-  !      F4   M4 o      C     o M2   F2
-  !              |            |
-  !              N1-----o-----N2
-  !                     M1
-  !                     F1 
-  ! N: node or vertex
-  ! F: face or edge
-  ! M: midpoint of the edge (o)
-  ! C: cell or control volume (in finite-volume lingo)
+  !<  Numbering convention for a 2D quadrilateral cell
+  !<
+  !<                     F3
+  !<                     M3
+  !<              N4-----o-----N3
+  !<              |            |
+  !<      F4   M4 o      C     o M2   F2
+  !<              |            |
+  !<              N1-----o-----N2
+  !<                     M1
+  !<                     F1
+  !< N: node or vertex
+  !< F: face or edge
+  !< M: midpoint of the edge (o)
+  !< C: cell or control volume (in finite-volume lingo)
 
   type quad_cell_t
     real(real64), dimension(4) :: x = 0.0_real64  !< vertex x coords
     real(real64), dimension(4) :: y = 0.0_real64  !< vertex y coords
     real(real64) :: volume = 0.0_real64 !< volume, aka area in 2d
 
-    real(real64), dimension(4,2,2) :: edge_norm_vectors = 0.0_real64 !< normal vectors at each face (face_id, x0:x1, y0:y1)
-    real(real64), dimension(4,2) :: edge_midpoints = 0.0_real64 !< midpoint of each edge (face_id, x, y)
+    real(real64), dimension(4, 2, 2) :: edge_norm_vectors = 0.0_real64 !< normal vectors at each face (face_id, x0:x1, y0:y1)
+    real(real64), dimension(4, 2) :: edge_midpoints = 0.0_real64 !< midpoint of each edge (face_id, x, y)
     real(real64), dimension(4) :: edge_lengths = 0.0_real64
   contains
     procedure, public :: initialize
@@ -56,16 +55,16 @@ contains
   subroutine calculate_edge_stats(self)
     class(quad_cell_t), intent(inout) :: self
 
-    associate(x => self%x, y => self%y)
+    associate(x=>self%x, y=>self%y)
       self%edge_lengths(1) = sqrt((x(2) - x(1))**2 + (y(2) - y(1))**2)
       self%edge_lengths(2) = sqrt((x(3) - x(2))**2 + (y(3) - y(2))**2)
       self%edge_lengths(3) = sqrt((x(4) - x(3))**2 + (y(4) - y(3))**2)
       self%edge_lengths(4) = sqrt((x(1) - x(4))**2 + (y(1) - y(4))**2)
 
-      self%edge_midpoints(1,:) = [0.5*(x(2) + x(1)), 0.5*(y(2) + y(1))]
-      self%edge_midpoints(2,:) = [0.5*(x(3) + x(2)), 0.5*(y(3) + y(2))]
-      self%edge_midpoints(3,:) = [0.5*(x(4) + x(3)), 0.5*(y(4) + y(3))]
-      self%edge_midpoints(4,:) = [0.5*(x(1) + x(4)), 0.5*(y(1) + y(4))]
+      self%edge_midpoints(1, :) = [0.5 * (x(2) + x(1)), 0.5 * (y(2) + y(1))]
+      self%edge_midpoints(2, :) = [0.5 * (x(3) + x(2)), 0.5 * (y(3) + y(2))]
+      self%edge_midpoints(3, :) = [0.5 * (x(4) + x(3)), 0.5 * (y(4) + y(3))]
+      self%edge_midpoints(4, :) = [0.5 * (x(1) + x(4)), 0.5 * (y(1) + y(4))]
     end associate
 
   end subroutine
@@ -73,52 +72,45 @@ contains
   subroutine calculate_volume(self)
     class(quad_cell_t), intent(inout) :: self
 
-    associate(v => self%volume, x => self%x, y => self%y)
+    associate(v=>self%volume, x=>self%x, y=>self%y)
       v = (x(2) - x(1)) * (y(4) - y(1)) - (x(4) - x(1)) * (y(2) - y(1))
     end associate
 
-    if (self%volume <= 0.0_real64) then
+    if(self%volume <= 0.0_real64) then
       error stop "Error: Negative volume (found at) "! // __LINE__ // " in " // __FILE__
     end if
 
   end subroutine
 
   subroutine calculate_edge_norm_vectors(self)
-    !< Find the vector normal to the edge originating at the midpoint 
+    !< Find the vector normal to the edge originating at the midpoint
 
     class(quad_cell_t), intent(inout) :: self
-    integer, dimension(4) :: head_idx = [2,3,4,1]
-    integer, dimension(4) :: tail_idx = [1,2,3,4]
+    integer, dimension(4) :: head_idx = [2, 3, 4, 1]
+    integer, dimension(4) :: tail_idx = [1, 2, 3, 4]
     integer :: i
     real(real64) :: dx, dy
 
+    do i = 1, 4
 
-    do i = 1,4
-
-      associate(x_tail => self%x(tail_idx(i)), &
-                x_mid =>self%edge_midpoints(i,1), &
-                x_head => self%x(head_idx(i)), &
-                y_tail => self%y(tail_idx(i)), &
-                y_mid =>self%edge_midpoints(i,2), &
-                y_head => self%y(head_idx(i)), &
-                n => self%edge_norm_vectors)
+      associate(x_tail=>self%x(tail_idx(i)), &
+                x_mid=>self%edge_midpoints(i, 1), &
+                x_head=>self%x(head_idx(i)), &
+                y_tail=>self%y(tail_idx(i)), &
+                y_mid=>self%edge_midpoints(i, 2), &
+                y_head=>self%y(head_idx(i)), &
+                n=>self%edge_norm_vectors)
 
         dx = x_head - x_mid
         dy = y_head - y_mid
 
-        n(i,1,:) = [x_mid, y_mid]
-        n(i,2,:) = [x_mid + dy, y_mid - dx]
-
-        print*, 'i', i
-        ! print*, n(i,1,:)
-        print*, n(i,2,:)
-        print*
-
+        n(i, 1, :) = [x_mid, y_mid]
+        n(i, 2, :) = [x_mid + dy, y_mid - dx]
 
       end associate
 
     end do
-    
+
   end subroutine
 
 end module mod_quad_cell
