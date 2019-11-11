e217b270d0fd2aa995f1d7e98f826abcf1800441
Miller, Samuel <smil@lle.rochester.edu>
2019-11-11 11:18:21 -0500

    Added a vector class and tests

--- src/lib/geometry/vector_2d.f90
+++ src/lib/geometry/vector_2d.f90
@@ -3,34 +3,72 @@ module mod_vector_2d
 
   implicit none
 
+  private
+  public :: vector_2d_t, operator(.unitnorm.)
+
   type vector_2d_t
     real(rk), dimension(2) :: x
     real(rk), dimension(2) :: y
-    real(rk), dimension(2) :: length
+    real(rk) :: length
   contains
     procedure, public :: initialize
-    procedure, public ::
+    ! procedure, private :: unit_normal
   end type
+
+  ! constructor
+  ! interface vector_2d_t
+  !   module procedure make_vector
+  ! end interface
 
   interface operator(.unitnorm.)
     module procedure unit_normal
   end interface
 
-  interface operator(.cross.)
-    module procedure cross_product
-  end interface
+  ! interface operator(.cross.)
+  !   module procedure cross_product
+  ! end interface
 
-  interface operator(.dot.)
-    module procedure d_product
-  end interface
+  ! interface operator(.dot.)
+  !   module procedure d_product
+  ! end interface
 
 contains
 
-  pure type(vector_2d_t) function vector(x, y) result(vec)
-    real(rk), intent(in), dimension(2) :: x
-    real(rk), intent(in), dimension(2) :: y
+  subroutine initialize(self, x_coords, y_coords)
+    class(vector_2d_t), intent(inout) :: self
+    !< Constructor for vector_2d_t
+    real(rk), intent(in), dimension(2) :: x_coords
+    real(rk), intent(in), dimension(2) :: y_coords
 
-    vec
+    self%x = x_coords
+    self%y = y_coords
+    self%length = sqrt((self%x(2) - self%x(1))**2 + (self%y(2) - self%y(1))**2)
+  end subroutine
+
+  ! pure type(vector_2d_t) function make_vector(x_coords, y_coords) result(vec)
+  !   !< Constructor for vector_2d_t
+  !   real(rk), intent(in), dimension(2) :: x_coords
+  !   real(rk), intent(in), dimension(2) :: y_coords
+
+  !   vec%x = x_coords
+  !   vec%y = y_coords
+  !   vec%length = sqrt((vec%x(2) - vec%x(1))**2 + (vec%y(2) - vec%y(1))**2)
+
+  ! end function
+
+  type(vector_2d_t) function unit_normal(vec) result(norm_vec)
+    !< Normalize a vector_2d_t to unit length
+    class(vector_2d_t), intent(in) :: vec
+
+    real(rk), dimension(2) :: x
+    real(rk), dimension(2) :: y
+    real(rk), dimension(2) :: vec_at_origin
+
+    vec_at_origin(:) = [vec%x(2) - vec%x(1), vec%y(2) - vec%y(1)]
+
+    vec_at_origin = vec_at_origin / norm2(vec_at_origin)
+    call norm_vec%initialize(x_coords=[vec%x(1), vec%x(1) + vec_at_origin(1)], y_coords=[vec%y(1), vec%y(1) + vec_at_origin(2)])
+
   end function
 
 end module mod_vector_2d
