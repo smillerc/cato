7f4d3f260c36d12ff68617014eeb32df1b428547
Miller, Samuel <smil@lle.rochester.edu>
2019-11-20 15:36:42 -0500

    Added an eos module

---
+++ src/lib/equation_of_state.f90
@@ -0,0 +1,47 @@
+module mod_eos
+
+  use iso_fortran_env, only: ik => int32, rk => real64
+  use mod_input, only: input_t
+  implicit none
+
+  private
+  public :: set_equation_of_state, eos
+
+  type :: eos_t
+    private
+    real(rk) :: gamma
+  contains
+    procedure, public :: get_gamma
+    procedure, public :: set_gamma
+  end type eos_t
+
+  interface eos_t
+    module procedure :: constructor
+  end interface
+
+  type(eos_t) :: eos !< singleton equation of state object
+
+contains
+  type(eos_t) function constructor(input) result(eq_of_state)
+    !< Constructor for the equation of state type
+    class(input_t), intent(in) :: input
+    call eq_of_state%set_gamma(input%polytropic_index)
+  end function
+
+  subroutine set_gamma(self, gamma)
+    class(eos_t), intent(inout) :: self
+    real(rk), intent(in) :: gamma
+    self%gamma = gamma
+  end subroutine
+
+  real(rk) pure function get_gamma(self) result(gamma)
+    class(eos_t), intent(in) :: self
+    gamma = self%gamma
+  end function
+
+  subroutine set_equation_of_state(input)
+    class(input_t), intent(in) :: input
+    eos = constructor(input)
+  end subroutine
+
+end module mod_eos
