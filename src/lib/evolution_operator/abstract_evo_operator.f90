ecd1a62bfc90e15fc6013114f40084eee8d6d720
Miller, Samuel <smil@lle.rochester.edu>
2019-11-20 15:38:02 -0500

    [WIP] Evolution operator updates

--- src/lib/evolution_operator/abstract_evo_operator.f90
+++ src/lib/evolution_operator/abstract_evo_operator.f90
@@ -3,6 +3,8 @@ module mod_abstract_evo_operator
 
   use iso_fortran_env, only: ik => int32, rk => real64
   use mod_abstract_reconstruction, only: abstract_reconstruction_t
+  use mod_mach_cone_geometry, only: mach_cone_geometry_t
+  use mod_grid, only: grid_t
 
   implicit none
 
@@ -10,28 +12,34 @@ module mod_abstract_evo_operator
   public :: abstract_evo_operator_t
 
   type, abstract :: abstract_evo_operator_t
+    class(grid_t), pointer :: grid !< pointer to the grid object
+    real(rk), dimension(:, :, :), pointer :: conserved_vars !< pointer to U
+    type(mach_cone_geometry_t) :: mach_cone !< Mach cone used to provide angles theta_ib and theta_ie
+    class(abstract_reconstruction_t), pointer :: reconstruction_operator !< pointer to the R_Omega operator used to provide values at the P' location
     character(:), allocatable :: name
-    real(rk), private :: tau !< time increment to evolve
+    real(rk) :: tau !< time increment to evolve
   contains
     procedure(evolve), deferred, public :: evolve
     procedure, public, non_overridable :: set_tau
   end type abstract_evo_operator_t
 
   abstract interface
-    function evolve(self, i, j, reconstruction_operator, edge, loc) result(U_bar)
+    pure function evolve(self, reconstructed_state, i, j, edge, loc) result(U_bar)
       !< Evolve the interface (edge) quantities based on their local reconstruction and neighboring cells
       import :: abstract_evo_operator_t
       import :: abstract_reconstruction_t
       import :: rk, ik
 
+      real(rk), dimension(4) :: U_bar !< Conserved variable values specified location
       class(abstract_evo_operator_t), intent(in) :: self
-      integer(ik), intent(in) :: i, j !< Cell indices
-      class(abstract_reconstruction_t), pointer, intent(in) :: reconstruction_operator
+      real(rk), dimension(:, :, :, :, :), intent(in) :: reconstructed_state
+      !< ([rho, u ,v, p], point, node/midpoint, i, j);
+      !< The node/midpoint dimension just selects which set of points,
+      !< e.g. 1 - all corners, 2 - all midpoints
 
-      ! Optional arguments (depending on the order of the reconstruction)
-      real(rk), dimension(4) :: U_bar !< Conserved variable values at the interface (edge)
-      integer(ik), intent(in), optional :: edge !< Cell edge index
-      character(len=3), intent(in), optional :: loc !< where is this evolution taking place? Needs to be one of the following ['k,1','k,c','k,2']
+      integer(ik), intent(in) :: i, j !< Cell indices
+      integer(ik), intent(in) :: edge !< Cell edge index
+      character(len=3), intent(in) :: loc !< where is this evolution taking place? Needs to be one of the following ['k,1','k,c','k,2']
     end function evolve
   end interface
 
