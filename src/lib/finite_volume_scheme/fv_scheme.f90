70f4a350d69c60605aa7f447f4c1300d8be8c011
Miller, Samuel <smil@lle.rochester.edu>
2019-11-20 15:38:32 -0500

    [WIP] FV scheme interface updates

--- src/lib/finite_volume_scheme/fv_scheme.f90
+++ src/lib/finite_volume_scheme/fv_scheme.f90
@@ -7,6 +7,7 @@ module mod_finite_volume_schemes
   use mod_integrand, only: integrand_t
   use mod_conserved_vars, only: conserved_vars_t
   use mod_grid, only: grid_t
+  use mod_abstract_evo_operator, only: abstract_evo_operator_t
 
   implicit none
   private
@@ -14,11 +15,24 @@ module mod_finite_volume_schemes
 
   type, abstract, extends(integrand_t) :: finite_volume_scheme_t
     class(grid_t), allocatable :: grid
-    class(conserved_vars_t), allocatable :: conserved_vars
     class(abstract_reconstruction_t), allocatable :: reconstruction_operator
+
+    real(rk), dimension(:, :, :), allocatable :: conserved_vars
+    !< Conserved variables U = [rho, u ,v, p]; indexed via ([rho, u ,v, p], i, j)
+
+    real(rk), dimension(:, :, :, :, :), allocatable :: reconstructed_state
+    !< ([rho, u ,v, p], point, node/midpoint, i, j); The node/midpoint dimension just selects which set of points,
+    !< e.g. 1 - all corners, 2 - all midpoints
+
+    real(rk), dimension(:, :, :, :, :), allocatable :: reference_state
+    !< ([rho, u ,v, p], point, node/midpoint, i, j)
+
+    class(abstract_evo_operator_t), allocatable :: evolution_operator
+
   contains
     procedure(reconstruct), public, deferred :: reconstruct
-    procedure(eval_fluxes), public, deferred :: eval_fluxes
+    ! procedure(eval_fluxes), public, deferred :: eval_fluxes
+    procedure, public :: calculate_reference_state
   end type
 
   abstract interface
@@ -27,13 +41,68 @@ module mod_finite_volume_schemes
       class(finite_volume_scheme_t), intent(inout) :: self
     end subroutine
 
-    pure function eval_fluxes(self) result(H)
+    pure function integrate_fluxes(self) result(rhs)
       import :: finite_volume_scheme_t
-      real(rk), dimension(2, 4) :: H !< Flux tensor
-      class(finite_volume_scheme_t), intent(inout) :: self
+      import :: rk
+
+      class(finite_volume_scheme_t), intent(in) :: self
+      real(rk), dimension(4) :: edge_flux
+      real(rk), dimension(size(self%conserved_vars, dim=1), &
+                          size(self%conserved_vars, dim=2), &
+                          size(self%conserved_vars, dim=3)) :: rhs !< RHS of Eq 3 in the main text
     end function
 
   end interface
 contains
+
+  subroutine calculate_reference_state(self)
+    !< Calculate the reference state at each corner/midpoint. This is just an average of
+    !< the neighboring cells
+    class(finite_volume_scheme_t), intent(inout) :: self
+    integer(ik) :: i, j
+
+    ! Midpoints
+    do j = self%grid%jlo, self%grid%jhi
+      do i = self%grid%ilo, self%grid%ihi
+        associate(U_tilde=>self%reference_state, U=>self%conserved_vars)
+          ! Midpoint 1 (bottom) ! = [rho,u,v,p]
+          U_tilde(:, 1, 2, i, j) = 0.5_rk * (U(:, i, j) + U(:, i, j - 1))
+
+          ! Midpoint 2 (right)
+          U_tilde(:, 2, 2, i, j) = 0.5_rk * (U(:, i, j) + U(:, i + 1, j))
+
+          ! Midpoint 3 (top)
+          U_tilde(:, 3, 2, i, j) = 0.5_rk * (U(:, i, j) + U(:, i, j + 1))
+
+          ! Midpoint 4 (left)
+          U_tilde(:, 4, 2, i, j) = 0.5_rk * (U(:, i, j) + U(:, i - 1, j))
+        end associate
+      end do
+    end do
+
+    ! Corners
+    do j = self%grid%jlo, self%grid%jhi
+      do i = self%grid%ilo, self%grid%ihi
+        associate(U_tilde=>self%reference_state, U=>self%conserved_vars)
+          ! Corner 1 (bottom-left)
+          U_tilde(:, 1, 1, i, j) = 0.25_rk * (U(:, i, j) + U(:, i, j - 1) + &
+                                              U(:, i - 1, j) + U(:, i - 1, j - 1))
+
+          ! Corner 2 (bottom-right)
+          U_tilde(:, 1, 1, i, j) = 0.25_rk * (U(:, i, j) + U(:, i, j - 1) + &
+                                              U(:, i + 1, j) + U(:, i + 1, j - 1))
+
+          ! Corner 3 (top-right)
+          U_tilde(:, 1, 1, i, j) = 0.25_rk * (U(:, i, j) + U(:, i, j + 1) + &
+                                              U(:, i + 1, j) + U(:, i + 1, j + 1))
+
+          ! Corner 4 (top-left)
+          U_tilde(:, 1, 1, i, j) = 0.25_rk * (U(:, i, j) + U(:, i, j + 1) + &
+                                              U(:, i - 1, j) + U(:, i - 1, j + 1))
+        end associate
+      end do
+    end do
+
+  end subroutine
 
 end module mod_finite_volume_schemes
--- src/lib/finite_volume_scheme/fvleg.f90
+++ src/lib/finite_volume_scheme/fvleg.f90
@@ -10,7 +10,8 @@ module mod_fvleg
   use mod_reconstruction_factory, only: reconstruction_factory_t
   use mod_local_evo_operator, only: local_evo_operator_t
   use mod_second_order_reconstruction, only: second_order_reconstruction_t
-  use mod_first_order_reconstruction, only: first_order_reconstruction_t
+  use mod_flux_tensor, only: H => flux_tensor_t
+  ! use mod_first_order_reconstruction, only: first_order_reconstruction_t
 
   implicit none
   private
@@ -18,16 +19,21 @@ module mod_fvleg
 
   type, extends(finite_volume_scheme_t) :: fvleg_t
     !< Implementation of the finite volume local evolution Galerkin (fvleg) scheme type
-    class(local_evo_operator_t), allocatable :: local_evolution_operator
+
   contains
     procedure, public :: reconstruct => reconstruct_fvleg
-    procedure, public :: eval_fluxes => eval_fvleg_fluxes
+    ! procedure, public :: eval_fluxes => eval_fvleg_fluxes
     procedure, public :: t => time_derivative
 
     procedure, public :: add => add_fvleg
     procedure, public :: multiply => multiply_fvleg
     procedure, public :: assign => assign_fvleg
+    final :: finalize
   end type
+
+  interface fvleg_t
+    module procedure :: constructor
+  end interface
 
 contains
 
@@ -36,101 +42,163 @@ contains
     class(input_t), intent(in) :: input
     class(fvleg_t), allocatable :: scheme
     type(reconstruction_factory_t) :: recon_factory
+    ! type(grid_factory_t) :: grid_factory
+    integer(ik) :: alloc_status
 
-    recon_factory = reconstruction_factory_t()
     allocate(fvleg_t :: scheme)
 
-    scheme%conserved_vars = conserved_vars_t(input%ni, input%nj)
-    scheme%reconstruction_operator = recon_factory%create_reconstruction(input)
+    ! grid_factory = grid_factory_t(input)
+    ! scheme%grid = grid_factory%create_grid()
+
+    ! recon_factory = reconstruction_factory_t(input)
+    ! scheme%reconstruction_operator = recon_factory%create_reconstruction(input)
+
+    allocate(scheme%conserved_vars(4, input%ni, input%nj), stat=alloc_status)
+    if(alloc_status /= 0) error stop "Unable to allocate fvleg_t%conserved_vars"
+
+    allocate(scheme%reconstructed_state(4, 4, 2, input%ni, input%nj), stat=alloc_status)
+    if(alloc_status /= 0) error stop "Unable to allocate fvleg_t%reconstructed_state"
+
+    allocate(scheme%reference_state(4, 4, 2, input%ni, input%nj), stat=alloc_status)
+    if(alloc_status /= 0) error stop "Unable to allocate fvleg_t%reference_state"
+
   end function
 
-  subroutine reconstruct_fvleg(self)
-    class(fvleg_t), intent(inout) :: self
+  subroutine finalize(self)
+    !< Implementation of the class cleanup
+    type(fvleg_t), intent(inout) :: self
+    integer(ik) :: alloc_status
+
+    if(allocated(self%grid)) deallocate(self%grid, stat=alloc_status)
+    if(alloc_status /= 0) error stop "Unable to deallocate fvleg_t%grid"
+
+    if(allocated(self%reconstruction_operator)) deallocate(self%reconstruction_operator, stat=alloc_status)
+    if(alloc_status /= 0) error stop "Unable to deallocate fvleg_t%reconstruction_operator"
+
+    if(allocated(self%conserved_vars)) deallocate(self%conserved_vars, stat=alloc_status)
+    if(alloc_status /= 0) error stop "Unable to deallocate fvleg_t%conserved_vars"
+
+    if(allocated(self%reconstructed_state)) deallocate(self%reconstructed_state, stat=alloc_status)
+    if(alloc_status /= 0) error stop "Unable to deallocate fvleg_t%reconstructed_state"
+
+    if(allocated(self%reference_state)) deallocate(self%reference_state, stat=alloc_status)
+    if(alloc_status /= 0) error stop "Unable to deallocate fvleg_t%reference_state"
   end subroutine
 
-  pure function get_fluxes(density, x_velocity, y_velocity, pressure, gamma) result(H)
-    !< Create the H tensor based on the given state variables
-    real(rk), intent(in) :: density
-    real(rk), intent(in) :: x_velocity
-    real(rk), intent(in) :: y_velocity
-    real(rk), intent(in) :: pressure
-    real(rk), intent(in) :: gamma
-    real(rk), dimension(2, 4) :: H
-
-    real(rk) :: total_energy, internal_energy
-
-    internal_energy = (pressure / density) / (gamma - 1)
-    total_energy = density * (internal_energy + 0.5_rk * (x_velocity**2 + y_velocity**2))
-
-    ! The flux tensor is H = Fi + Gj
-    associate(rho=>density, u=>x_velocity, v=>y_velocity, p=>pressure, E=>total_energy)
-      ! F
-      H(1) = [rho * u, rho * u**2 + p, rho * u * v,(E + p) * u]
-
-      ! G
-      H(2) = [rho * v, rho * u * v, rho * v**2 + p,(E + p) * v]
-    end associate
-  end function
-
-  pure function eval_fvleg_fluxes(self) result(H)
-
-    !< Evaluate the fluxes in the equation dU/dt + dF/dx + dG/dy = 0, where H = Fi + Gj
+  subroutine reconstruct_fvleg(self)
+    !< Implementation of the FVLEG reconstruction.
+    !< This reconstructs the entire grid at all the nodes/midpoints
     class(fvleg_t), intent(inout) :: self
-    real(rk), dimension(2, 4) :: H
-    real(rk), dimension(4) :: F, G
 
-  end function
+    call self%reconstruction_operator%reconstruct_domain(self%reconstructed_state)
+
+    ! do j = self%grid%jlo, self%grid%jhi
+    !   do i = self%grid%ilo, self%grid%ihi
+
+    !     ! The cell average (and gradient if higher order) is resused for each cell
+    !     call self%reconstruction_operator%select_and_find_gradient(i,j)
+
+    !     ! First do corners, then to midpoints
+    !     do n = 1, ubound(self%grid%reconstructed_state, dim=3)
+
+    !       ! Loop through each point (N1-N4, and M1-M4)
+    !       do p = 1, ubound(self%grid%reconstructed_state, dim=2)
+
+    !         associate(V_bar => self%reconstructed_state, &
+    !                   R_omega => self%reconstruction_operator%reconstruct, &
+    !                   xy => self%grid%cell_node_xy(:, p, n, i, j))
+
+    !           ! reconstructed_state(rho:p, point, node/midpoint, i, j)
+    !           V_bar(:, p, n, i, j) = R_omega(xy)
+    !         end associate
+
+    !       end do
+    !     end do
+    !   end do
+    ! end do
+
+  end subroutine
 
   function time_derivative(self) result(dU_dt)
-    !< Implementation of dU/dt
+    !< Implementation of dU/dt = 1/Omega_ij Sum(1,k) Integral(H . n dl)
     class(fvleg_t), intent(in) :: self
     class(integrand_t), allocatable :: dU_dt
     type(fvleg_t), allocatable :: local_dU_dt
+    integer(ik) :: alloc_status
 
-    ! allocate(local_dU_dt)
+    allocate(local_dU_dt, stat=alloc_status)
+    if(alloc_status /= 0) error stop "Unable to allocate local_dU_dt"
 
-    ! select type(self%reconstruction_operator)
-    ! class is (first_order_reconstruction_t)
-    !   dU_dt = self%first_order_edge_flux()
-    ! class is (second_order_reconstruction_t)
-    !   dU_dt = self%second_order_edge_flux()
-    ! end select
+    local_dU_dt%grid = self%grid
+    local_dU_dt%evolution_operator = self%evolution_operator
+    local_dU_dt%reconstruction_operator = self%reconstruction_operator
+
+    allocate(local_dU_dt%reconstructed_state(size(self%reconstructed_state, dim=1), &
+                                             size(self%reconstructed_state, dim=2), &
+                                             size(self%reconstructed_state, dim=3), &
+                                             size(self%reconstructed_state, dim=4), &
+                                             size(self%reconstructed_state, dim=5)), &
+             stat=alloc_status)
+    if(alloc_status /= 0) error stop "Unable to allocate local_dU_dt%reconstructed_state"
+
+    allocate(local_dU_dt%reference_state(size(self%reference_state, dim=1), &
+                                         size(self%reference_state, dim=2), &
+                                         size(self%reference_state, dim=3), &
+                                         size(self%reference_state, dim=4), &
+                                         size(self%reference_state, dim=5)), &
+             stat=alloc_status)
+    if(alloc_status /= 0) error stop "Unable to allocate local_dU_dt%reference_state"
+
+    allocate(local_dU_dt%conserved_vars(size(self%conserved_vars, dim=1), &
+                                        size(self%conserved_vars, dim=2), &
+                                        size(self%conserved_vars, dim=3)), &
+             stat=alloc_status)
+    if(alloc_status /= 0) error stop "Unable to allocate local_dU_dt%conserved_vars"
+
+    ! Set the RHS of Eq. 3 in the main text
+    ! local_dU_dt%conserved_vars = self%integrate_fluxes() //TODO: fix
+
+    call move_alloc(local_dU_dt, dU_dt)
 
   end function time_derivative
 
-  pure function first_order_edge_flux(self) result(H)
-    class(fvleg_t), intent(inout) :: self
-    real(rk), dimension(2, 4) :: H
-
-  end function first_order_edge_flux
-
-  pure function second_order_edge_flux(self) result(H)
+  pure function integrate_fluxes(self) result(rhs)
     !< Evaluate the fluxes along the edges. This is equation 13 in the paper
-    class(fvleg_t), intent(inout) :: self
-    real(rk), dimension(2, 4) :: H
-
+    use mod_flux_tensor, only: operator(.dot.)
+    class(fvleg_t), intent(in) :: self
+    real(rk), dimension(4) :: edge_flux
+    real(rk), dimension(size(self%conserved_vars, dim=1), &
+                        size(self%conserved_vars, dim=2), &
+                        size(self%conserved_vars, dim=3)) :: rhs !< RHS of Eq 3 in the main text
+    ! type(flux_tensor_t) :: H1, H2, H3
     integer(ik) :: i, j, k
 
     do j = self%grid%jlo, self%grid%jhi
       do i = self%grid%ilo, self%grid%ihi
 
-        r_omega_u_bar => self%reconstruction_operator
         edge_flux = 0.0_rk
 
         do k = 1, 4  ! edge
-          associate(H=>self%get_fluxes, E0=>self%local_evolution_operator%evolve, &
-                    n_hat=>self%grid%cell_edge_norm_vectors(i, j, k, :, :), &
-                    delta_l=>self%grid%cell_edge_lengths(i, j, k))
-            edge_flux = edge_flux + &
-                        H(E0(r_omega_u_bar, edge=k, loc='k,1'))) + &
-                        4 * H(E0(r_omega_u_bar, edge=k, loc='k,c'))) + &
-                        H(E0(r_omega_u_bar, edge=k, loc='k,2'))) .dot.n_hat * delta_l / 6.0_rk
-          end associate
-        end do
-      end do
-    end do
+          associate(E0=>self%evolution_operator, &
+                    r_omega_u_bar=>self%reconstructed_state, &
+                    n_hat=>self%grid%cell_edge_norm_vectors(:, k, i, j), &
+                    delta_l=>self%grid%cell_edge_lengths(k, i, j))
 
-  end function second_order_edge_flux
+            edge_flux = edge_flux + &
+                        ( &
+                        ((H(E0%evolve(r_omega_u_bar, i, j, edge=k, loc='k,1')) + &
+                          H(E0%evolve(r_omega_u_bar, i, j, edge=k, loc='k,c')) * 4.0_rk + &
+                          H(E0%evolve(r_omega_u_bar, i, j, edge=k, loc='k,2'))) .dot.n_hat) * (delta_l / 6.0_rk))
+
+          end associate
+        end do ! k
+
+        rhs(:, i, j) = (-1.0_rk / self%grid%cell_volume(i, j)) * edge_flux
+
+      end do ! i
+    end do ! j
+
+  end function integrate_fluxes
 
   function add_fvleg(lhs, rhs) result(operator_result)
     class(fvleg_t), intent(in) :: lhs
