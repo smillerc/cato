059145889d893eaf373256adeefd9f33b364ca87
Miller, Samuel <smil@lle.rochester.edu>
2019-12-04 10:15:39 -0500

    Updated the clean script  - Now runs on all .f90 and .pf files

--- scripts/clean_src.sh
+++ scripts/clean_src.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
-fprettify *.f90 --indent 2 --whitespace 3 --whitespace-intrinsics False --strict-indent --enable-replacements --c-relations
+cd ../src
+find . -name "*.f90"|while read fname; do
+  echo "$fname"
+  fprettify ${fname} --indent 2 --whitespace 3 --whitespace-intrinsics False --strict-indent --enable-replacements --c-relations
+done
+
+cd ../tests
+find . -name "*.pf"|while read fname; do
+  echo "$fname"
+  fprettify ${fname} --indent 2 --whitespace 3 --whitespace-intrinsics False --strict-indent --enable-replacements --c-relations
+done
+
+cd ..
