2570ec68d9eec58d16078f7210b0a7071136e201
Miller, Samuel <smil@lle.rochester.edu>
2019-12-13 14:35:15 +0000

    Added a global debug print option

--- src/lib/globals.f90
+++ src/lib/globals.f90
@@ -1,7 +1,10 @@
 module mod_globals
-  use iso_fortran_env, only: compiler_options, compiler_version, output_unit
+  use iso_fortran_env, only: compiler_options, compiler_version, std_out => output_unit
 
   implicit none
+
+  logical, parameter :: enable_debug_print = .true.
+  logical, parameter :: enable_file_and_line_stats = .true.
 
   logical :: globals_set = .false.
   character(:), allocatable :: compiler_flags_str
@@ -26,18 +29,40 @@ contains
   subroutine print_version_stats()
     call set_global_options()
     ! if(this_image() == 1) then
-    write(output_unit, '(a,a)')
-    write(output_unit, '(a,a)') "Version: ", fvleg_2d_version
-    write(output_unit, '(a,a)') "Build Type: ", build_type
-    write(output_unit, '(a,a)') "Compile OS: ", compile_os
-    write(output_unit, '(a,a)') "Compiler Flags: ", compiler_flags_str
-    write(output_unit, '(a,a)') "Compiler Version: ", compiler_version_str
-    write(output_unit, '(a,a)') "Git Hash: ", git_hash
-    write(output_unit, '(a,a)') "Git Ref: ", git_ref
-    write(output_unit, '(a,a)') "Commited Changes (Clean/Dirty): ", git_local_changes
-    write(output_unit, '(a,a)') "Compile Host: ", compile_host
-    write(output_unit, '(a,a)')
+    write(std_out, '(a,a)')
+    write(std_out, '(a,a)') "Version: ", fvleg_2d_version
+    write(std_out, '(a,a)') "Build Type: ", build_type
+    write(std_out, '(a,a)') "Compile OS: ", compile_os
+    write(std_out, '(a,a)') "Compiler Flags: ", compiler_flags_str
+    write(std_out, '(a,a)') "Compiler Version: ", compiler_version_str
+    write(std_out, '(a,a)') "Git Hash: ", git_hash
+    write(std_out, '(a,a)') "Git Ref: ", git_ref
+    write(std_out, '(a,a)') "Commited Changes (Clean/Dirty): ", git_local_changes
+    write(std_out, '(a,a)') "Compile Host: ", compile_host
+    write(std_out, '(a,a)')
     ! end if
+  end subroutine
+
+  subroutine debug_print(str, file, line_number)
+    character(len=*), intent(in) :: str
+    character(len=*), intent(in), optional :: file
+    integer, intent(in), optional :: line_number
+
+    if(enable_debug_print) then
+      if(this_image() == 1) then
+
+        if(enable_file_and_line_stats) then
+          if(present(file) .and. present(line_number)) then
+            write(std_out, '(a,a,i0,3(a))') file, ':', line_number, ' "', str, '"'
+          else
+            write(std_out, '(a)') str
+          end if
+        else
+          write(std_out, '(a)') str
+        end if
+
+      end if
+    end if
   end subroutine
 
 end module mod_globals
