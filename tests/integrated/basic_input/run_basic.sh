1ee3b06741d6420a366b2877ddc7ee3926118c35
Miller, Samuel <smil@lle.rochester.edu>
2019-12-16 13:46:42 -0500

    Integrated test updates

--- tests/integrated/basic_input/run_basic.sh
+++ tests/integrated/basic_input/run_basic.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 rm fvleg_2d.x
-cd ../../../build && make -j
-cd -
-cp ../../../build/bin/fvleg_2d.x .
+cd ../../../build &&\
+make -j &&\
+cd - &&\
+cp ../../../build/bin/fvleg_2d.x . &&\
 ./fvleg_2d.x basic.ini
--- tests/integrated/double_shear/run.sh
+++ tests/integrated/double_shear/run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-module purge
-module load pfunit
-module load cgns
-module load intel/2019.3
+#module purge
+#module load pfunit
+#module load cgns
+#module load intel/2019.3
 
 export I_MPI_REMOVED_VAR_WARNING=0
 export I_MPI_VAR_CHECK_SPELLING=0
