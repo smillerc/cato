8f5cb90bc5b28c581143e27c85ebda6bcd1c293d
Miller, Samuel <smil@lle.rochester.edu>
2020-01-10 13:00:17 -0500

    Added new functions to the eos class  - speed of sound, isentropic p2/p1, etc.

--- src/lib/equation_of_state.f90
+++ src/lib/equation_of_state.f90
@@ -13,6 +13,11 @@ module mod_eos
   contains
     procedure, public :: get_gamma
     procedure, public :: set_gamma
+    procedure, public :: calc_pressure_from_dens_and_internal_energy
+    procedure, public :: calc_internal_energy_from_press_and_dens
+    procedure, public :: calc_total_energy
+    procedure, public :: calc_sound_speed
+    procedure, public :: calc_density_from_isentropic_press
   end type eos_t
 
   interface eos_t
@@ -44,8 +49,62 @@ contains
     eos = constructor(input)
   end subroutine
 
-  ! elemental function density_from_pressure(pressure) result(density)
-  ! end function
-  ! internal_energy = (p / rho) / (gamma - 1)
-  ! total_energy = rho * (internal_energy + 0.5_rk * (u**2 + v**2))
+  elemental function calc_pressure_from_dens_and_internal_energy(self, density, internal_energy) result(pressure)
+    !< Ideal gas law -> P = (1-gamma)*rho*e
+    class(eos_t), intent(in) :: self
+    real(rk) :: pressure
+    real(rk), intent(in) :: density
+    real(rk), intent(in) :: internal_energy
+
+    pressure = (1.0_rk - self%gamma) * density * internal_energy
+  end function
+
+  elemental function calc_internal_energy_from_press_and_dens(self, pressure, density) result(internal_energy)
+    !< Calculate internal energy
+    class(eos_t), intent(in) :: self
+    real(rk), intent(in) :: pressure
+    real(rk), intent(in) :: density
+    real(rk) :: internal_energy
+
+    internal_energy = (pressure / density) / (self%gamma - 1.0_rk)
+  end function
+
+  elemental function calc_total_energy(self, internal_energy, density, x_velocity, y_velocity) result(total_energy)
+    !< Calculate total energy
+    class(eos_t), intent(in) :: self
+    real(rk), intent(in) :: internal_energy
+    real(rk), intent(in) :: density
+    real(rk), intent(in) :: x_velocity
+    real(rk), intent(in) :: y_velocity
+    real(rk) :: total_energy
+
+    total_energy = density * (internal_energy + 0.5_rk * (x_velocity**2 + y_velocity**2))
+  end function
+
+  elemental function calc_sound_speed(self, pressure, density) result(sound_speed)
+    !< Calculate sound speed
+    class(eos_t), intent(in) :: self
+    real(rk), intent(in) :: pressure
+    real(rk), intent(in) :: density
+    real(rk) :: sound_speed
+
+    real(rk) :: sqrt_term
+
+    if(pressure < 0) error stop "Pressure is < 0 in eos_t%calc_sound_speed"
+    if(density < 0) error stop "Density is < 0 in eos_t%calc_sound_speed"
+
+    sqrt_term = self%gamma * pressure / density
+    sound_speed = sqrt(sqrt_term)
+  end function
+
+  elemental function calc_density_from_isentropic_press(self, P_1, P_2, rho_1) result(rho_2)
+    !< Calculate the isentropic density based on a given pressure ratio and initial density
+    class(eos_t), intent(in) :: self
+    real(rk), intent(in) :: P_1
+    real(rk), intent(in) :: P_2
+    real(rk), intent(in) :: rho_1
+    real(rk) :: rho_2
+
+    rho_2 = rho_1 * (P_2 / P_1)**(self%gamma - 1)
+  end function
 end module mod_eos
