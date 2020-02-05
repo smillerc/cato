b1b74e685a30107d03d7edc41f4c680f6802d32f
Miller, Samuel <smil@lle.rochester.edu>
2020-02-05 10:49:16 -0500

    Added a 1d layered grid generation method in the python scripts

--- scripts/generate_initial_grids.py
+++ scripts/generate_initial_grids.py
@@ -4,6 +4,9 @@ in the .h5 format
 """
 import numpy as np
 import h5py
+import pint
+
+ureg = pint.UnitRegistry()
 
 
 def make_uniform_grid(n_cells, xrange, yrange):
@@ -65,6 +68,112 @@ def make_uniform_grid(n_cells, xrange, yrange):
     }
 
 
+def make_1d_layered_grid(
+    layer_thicknesses, layer_n_cells, layer_density, layer_u, layer_v, layer_pressure
+):
+    """Create a 1d layered grid
+
+    Parameters
+    ----------
+    layer_thicknesses : 1d array
+        Array with units for the thickness of each layer
+    layer_n_cells : list
+        List with the number of cells for each layer
+    layer_density : 1d array
+        Array with units for the density of each layer
+    layer_u : 1d array
+        Array with units for the x velocity of each layer
+    layer_v : 1d array
+        Array with units for the y velocity of each layer
+    layer_pressure : 1d array
+        Array with units for the pressure of each layer
+
+    Returns
+    -------
+    dictionary
+        A dictionary that contains the conserved variables (rho, u velocity, v
+        velocity, p), grid points (x, y), and the cell center (xc, yc) points
+    """
+
+    cumulative_thickness = 0
+    cumulative_ncells = 1  # start at 1 due to the ghost cell
+    node_x = []
+    layer_cell_idx_ranges = []  # [(first cell, last cell), etc]
+
+    layer_thicknesses = layer_thicknesses.to("cm").m
+    for ncells, thickness in zip(layer_n_cells, layer_thicknesses):
+        start_x = cumulative_thickness
+        stop_x = start_x + thickness
+        cumulative_thickness += thickness
+
+        start = cumulative_ncells
+        stop = start + ncells
+        layer_cell_idx_ranges.append((start, stop - 1))
+        cumulative_ncells += ncells
+
+        layer_x = np.linspace(start_x, stop_x, ncells + 1, endpoint=True)
+        node_x.append(layer_x)
+
+    # X location of all of the nodes/vertices
+    x = np.concatenate([xl[:-1] for xl in node_x] + [[node_x[-1][-1]]])
+
+    # right/left cell size - for making ghost layers
+    ldx = x[1] - x[0]
+    rdx = x[-1] - x[-2]
+
+    # Since this is a "1d" grid, it needs 3 cells in y
+    # (1 on either side for ghost layers)
+    y = np.array([-ldx, 0, ldx, ldx * 2], dtype=np.float64) - ldx / 2
+
+    # new x location for nodes/vertices including ghost cells
+    x = np.array([x[0] - ldx] + list(x) + [x[-1] + rdx], dtype=np.float64)
+
+    # 2d versions
+    x_2d, y_2d = np.meshgrid(x, y)  # nodes
+
+    # cell-centered arrays
+    rho = np.zeros((3, x.shape[0] - 1))
+    u = np.zeros((3, x.shape[0] - 1))
+    v = np.zeros((3, x.shape[0] - 1))
+    p = np.zeros((3, x.shape[0] - 1))
+
+    # Assign rho, u, v, and p to the cell-centered arrays
+    for l, layer in enumerate(layer_cell_idx_ranges):
+        s = layer[0]
+        e = layer[1] + 1
+
+        # Convert to cgs units
+        rho[:, s:e] = layer_density[l].to("g/cc").m
+        u[:, s:e] = layer_u[l].to("cm/s").m
+        v[:, s:e] = layer_v[l].to("cm/s").m
+        p[:, s:e] = layer_pressure[l].to("barye").m
+
+    # Assign ghost layer values
+    rho[:, 0] = layer_density[0].to("g/cc").m
+    p[:, 0] = layer_pressure[0].to("barye").m
+    rho[:, -1] = layer_density[-1].to("g/cc").m
+    p[:, -1] = layer_pressure[-1].to("barye").m
+
+    # cell spacing
+    dy = (np.diff(y_2d[:, 0]) / 2.0)[0]
+    dx = (np.diff(x_2d[0, :]) / 2.0)[0]
+
+    # cell center locations
+    xc = x_2d[:-1, :-1] + dx
+    yc = y_2d[:-1, :-1] + dy
+
+    return {
+        "x": x_2d,
+        "y": y_2d,
+        "rho": rho,
+        "u": u,
+        "v": v,
+        "p": p,
+        "xc": xc,
+        "yc": yc,
+    }
+
+
 def make_1d_in_x_uniform_grid(n_cells, limits=(0, 1)):
     """Generate a uniform grid 1d grid in x. This will output a dictionary
     that contains the appropriate arrays, which include the ghost
@@ -91,19 +200,24 @@ def make_1d_in_x_uniform_grid(n_cells, limits=(0, 1)):
 
     y = np.array([-ldx, 0, ldx, ldx * 2], dtype=np.float64) - ldx / 2
 
+    # make empty arrays for the cell center locations
     xc = np.zeros(x.shape[0] - 1, dtype=np.float64)
     yc = np.zeros(y.shape[0] - 1, dtype=np.float64)
 
-    xx, yy = np.meshgrid(x, y)
-    xxc, yyc = np.meshgrid(xc, yc)
+    # 2d versions
+    xx, yy = np.meshgrid(x, y)  # nodes
+    xxc, yyc = np.meshgrid(xc, yc)  # cell centers
 
     rho = np.ones_like(xxc, dtype=np.float64)
     pressure = np.ones_like(xxc, dtype=np.float64)
     u = np.ones_like(xxc, dtype=np.float64)
     v = np.ones_like(xxc, dtype=np.float64)
 
+    # cell spacing
     dy = (np.diff(yy[:, 0]) / 2.0)[0]
     dx = (np.diff(xx[0, :]) / 2.0)[0]
+
+    # cell center locations
     xc = xx[:-1, :-1] + dx
     yc = yy[:-1, :-1] + dy
 
