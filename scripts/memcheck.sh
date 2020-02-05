373e54e5e9586438cc5c328cf4ba0448edd78109
Miller, Samuel <smil@lle.rochester.edu>
2020-02-05 10:52:02 -0500

    Added profile and valgrind memory check bash scripts

---
+++ scripts/memcheck.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Run this script inside a
+export I_MPI_REMOVED_VAR_WARNING=0
+export I_MPI_VAR_CHECK_SPELLING=0
+export FOR_COARRAY_NUM_IMAGES=1
+
+BUILD_DIR=../../../build
+
+rm cato.x
+rm -rf step*
+cd ${BUILD_DIR} &&\
+make -j &&\
+cd - &&\
+cp ${BUILD_DIR}/bin/cato.x . &&\
+valgrind \
+    --leak-check=full \
+    --leak-resolution=high \
+    --show-reachable=yes \
+    --track-origins=yes \
+    --log-file="log_mcheck.txt" ./cato.x input.ini
