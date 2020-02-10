618f3efbad73dfad2022829d5cddb1b143444229
Miller, Samuel <smil@lle.rochester.edu>
2020-02-10 11:40:12 -0500

    Slope limiter rework/update  - Added more standard CFD slope limiters  - Reworked to symmetric slope form (rather than standard flux form)

--- src/lib/limiters/slope_limiter.f90
+++ src/lib/limiters/slope_limiter.f90
@@ -1,6 +1,15 @@
 module mod_slope_limiter
+  !< Summary: Define the various slope limiters
+  !< Note: The slope limiters in the form listed below is based on [1] and the symmetric forms
+  !<       of the slope (not flux) limiters. Slope and flux limiters are very similar, but
+  !<       have some subtle differences. See the reference for more info. The formulas are based on Eq 10.
+  !< References:
+  !< [1] M. Berger, M. Aftosmis, S. Muman, "Analysis of Slope Limiters on Irregular Grids",
+  !<     43rd AIAA Aerospace Sciences Meeting and Exhibit (2005), https://doi.org/10.2514/6.2005-490
+
   use, intrinsic :: iso_fortran_env, only: ik => int32, rk => real64
   use mod_floating_point_utils, only: equal
+  use math_constants, only: pi
 
   implicit none
 
@@ -19,32 +28,35 @@ module mod_slope_limiter
   end interface
 
   interface
-    pure function limit(a, b) result(slope)
+    pure real(rk) function limit(f) result(slope)
       import :: rk
-      real(rk) :: slope
-      real(rk), intent(in) :: a, b
+      real(rk), intent(in) :: f
     end function
   end interface
 
 contains
 
   type(slope_limiter_t) pure function constructor(name) result(limiter)
+    !< Slope limiter factory/constructor
     character(len=*), intent(in) :: name
 
     limiter%name = trim(name)
 
     select case(trim(name))
-    case('upwind')
-      limiter%limit => upwind_limit
+    case('barth_jesperson')
+      limiter%limit => barth_jespersen
     case('minmod')
-      limiter%limit => minmod_limit
-    case('weno')
-      limiter%limit => weno_limit
+      limiter%limit => minmod
+    case('van_leer')
+      limiter%limit => van_leer
+    case('sine')
+      limiter%limit => sine
+    case('van_albada')
+      limiter%limit => van_albada
     case default
-      limiter%limit => upwind_limit
-      limiter%name = 'upwind'
+      limiter%limit => minmod
+      limiter%name = 'minmod'
     end select
-
   end function
 
   subroutine finalize(self)
@@ -53,57 +65,39 @@ contains
     if(allocated(self%name)) deallocate(self%name)
   end subroutine finalize
 
-  pure function upwind_limit(a, b) result(slope)
-    !< Slope limiter based on Equation 10 in DOI: 10.1016/j.jcp.2009.04.001
-    !< Finds the equation of $$L(a,b) = \frac{max(ab,0)(a+b)}{a^2+b^2}$$
+  pure real(rk) function barth_jespersen(f) result(slope)
+    !< Barth Jesperson slope limiter. See Eq. 10 in [1]
+    real(rk), intent(in) :: f !< f  = (u_i - u_{i-1}) / (u_{i+1} - u_{i-1})
 
-    real(rk) :: slope
-    real(rk), intent(in) :: a, b
+    slope = min(1.0_rk, 4.0_rk * f, 4.0_rk * (1.0_rk - f))
+  end function barth_jespersen
 
-    real(rk) :: denom
+  pure real(rk) function van_leer(f) result(slope)
+    !< van Leer slope limiter. See Eq. 10 in [1]
+    real(rk), intent(in) :: f !< f  = (u_i - u_{i-1}) / (u_{i+1} - u_{i-1})
 
-    denom = a**2 + b**2
+    slope = 4.0_rk * f * (1.0_rk - f)
+  end function van_leer
 
-    if(denom > 0.0_rk) then
-      slope = (max(a * b, 0.0_rk) * (a + b)) / denom
-    else
-      slope = 0.0_rk
-    end if
+  pure real(rk) function sine(f) result(slope)
+    !< Sine slope limiter. See Eq. 10 in [1]
+    real(rk), intent(in) :: f !< f  = (u_i - u_{i-1}) / (u_{i+1} - u_{i-1})
 
-  end function upwind_limit
+    slope = sin(pi * f)
+  end function sine
 
-  pure function minmod_limit(a, b) result(slope)
-    real(rk) :: slope
-    real(rk), intent(in) :: a, b
+  pure real(rk) function van_albada(f) result(slope)
+    !< van Albada slope limiter. See Eq. 10 in [1]
+    real(rk), intent(in) :: f !< f  = (u_i - u_{i-1}) / (u_{i+1} - u_{i-1})
 
-    if(a * b > 0.0_rk) then
-      if(abs(a) < abs(b)) then
-        slope = a
-      else if(abs(b) < abs(a)) then
-        slope = b
-      else if(equal(abs(a), abs(b))) then
-        slope = a
-      end if
-    else
-      slope = 0.0_rk
-    end if
+    slope = (2.0_rk * f * (1.0_rk - f)) / (f**2 + (1.0_rk - f)**2)
+  end function van_albada
 
-  end function
+  pure real(rk) function minmod(f) result(slope)
+    !< Min-mod slope limiter. See Eq. 10 in [1]
+    real(rk), intent(in) :: f !< f  = (u_i - u_{i-1}) / (u_{i+1} - u_{i-1})
 
-  pure function weno_limit(a, b) result(slope)
-    !< Basic WENO slope limiter
-
-    real(rk) :: slope
-    real(rk), intent(in) :: a, b
-    real(rk) :: omega_1, omega_2  !< weights
-    real(rk) :: epsilon
-
-    epsilon = 1e-6_rk
-    omega_1 = (epsilon + a**2)**(-2)
-    omega_2 = (epsilon + b**2)**(-2)
-
-    slope = (omega_1 * a + omega_2 * b) / (omega_1 + omega_2)
-
-  end function
+    slope = min(2.0_rk * f, 2 * (1.0_rk - f))
+  end function minmod
 
 end module mod_slope_limiter
