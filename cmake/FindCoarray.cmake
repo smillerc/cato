1198b640f7afd8041f12e2f2daf98b816e029b57
Miller, Samuel <smil@lle.rochester.edu>
2020-04-10 09:05:42 -0400

    Cmake updates  - pulled from coarray branch  - disabled coarray flags for now

--- cmake/FindCoarray.cmake
+++ cmake/FindCoarray.cmake
@@ -1,5 +1,5 @@
-# Distributed under the OSI-approved BSD 3-Clause License.  See accompanying
-# file Copyright.txt or https://cmake.org/licensing for details.
+# Distributed under the OSI-approved BSD 3-Clause License.  See accompanying file Copyright.txt or
+# https://cmake.org/licensing for details.
 
 #[=======================================================================[.rst:
 FindCoarray
@@ -30,7 +30,7 @@ Result Variables
   maximum number of parallel processes
 ``Coarray_NUMPROC_FLAG``
   use for executing in parallel: ${Coarray_EXECUTABLE} ${Coarray_NUMPROC_FLAG} ${Coarray_MAX_NUMPROCS} ${CMAKE_CURRENT_BINARY_DIR}/myprogram
-  
+
 Cache Variables
 ^^^^^^^^^^^^^^^^
 
@@ -42,8 +42,8 @@ The following cache variables may also be set:
 
 cmake_policy(VERSION 3.3)
 
-set(options_coarray Intel)  # flags needed
-set(opencoarray_supported GNU)  # future: Flang, etc.
+set(options_coarray Intel) # flags needed
+set(opencoarray_supported GNU) # future: Flang, etc.
 
 unset(Coarray_COMPILE_OPTIONS)
 unset(Coarray_LIBRARY)
@@ -52,11 +52,31 @@ unset(Coarray_REQUIRED_VARS)
 if(CMAKE_Fortran_COMPILER_ID IN_LIST options_coarray)
 
   if(CMAKE_Fortran_COMPILER_ID STREQUAL Intel)
-    set(Coarray_COMPILE_OPTIONS -coarray=shared)
-    set(Coarray_LIBRARY -coarray=shared)  # ifort requires it at build AND link
+
+    if(WIN32)
+      set(Coarray_COMPILE_OPTIONS /Qcoarray:shared)
+      list(APPEND Coarray_REQUIRED_VARS ${Coarray_COMPILE_OPTIONS})
+    elseif(UNIX AND NOT APPLE)
+
+      if(SERIAL_BUILD)
+        set(IFORT_COARRAY "-coarray=single")
+        set(Coarray_MAX_NUMPROCS 1)
+      elseif(SHARED_MEMORY)
+        # if(ENABLE_TESTING)
+        #   set(IFORT_COARRAY "-coarray=shared -coarray-num-images=${N_CORES}")
+        # else()
+          set(IFORT_COARRAY "-coarray=shared")
+          set(Coarray_MAX_NUMPROCS ${N_CORES})
+        # endif()
+      elseif(DISTRIBUTED_MEMORY)
+        set(IFORT_COARRAY "-coarray=distributed")
+      endif()
+
+      set(Coarray_COMPILE_OPTIONS ${IFORT_COARRAY})
+      set(Coarray_LIBRARY ${IFORT_COARRAY}) # ifort requires it at build AND link
+      list(APPEND Coarray_REQUIRED_VARS ${Coarray_LIBRARY})
+    endif()
   endif()
-  
-  list(APPEND Coarray_REQUIRED_VARS ${Coarray_LIBRARY})
 
 elseif(CMAKE_Fortran_COMPILER_ID IN_LIST opencoarray_supported)
 
@@ -68,10 +88,10 @@ elseif(CMAKE_Fortran_COMPILER_ID IN_LIST opencoarray_supported)
     set(Coarray_EXECUTABLE cafrun)
 
     include(ProcessorCount)
-    ProcessorCount(Nproc)
+    processorcount(Nproc)
     set(Coarray_MAX_NUMPROCS ${Nproc})
     set(Coarray_NUMPROC_FLAG -np)
-    
+
     list(APPEND Coarray_REQUIRED_VARS ${Coarray_LIBRARY})
   elseif(CMAKE_Fortran_COMPILER_ID STREQUAL GNU)
     set(Coarray_COMPILE_OPTIONS -fcoarray=single)
@@ -80,24 +100,24 @@ elseif(CMAKE_Fortran_COMPILER_ID IN_LIST opencoarray_supported)
 
 endif()
 
-
 set(CMAKE_REQUIRED_FLAGS ${Coarray_COMPILE_OPTIONS})
 set(CMAKE_REQUIRED_LIBRARIES ${Coarray_LIBRARY})
 include(CheckFortranSourceCompiles)
-# check_fortran_source_compiles("program cs; real :: x[*]; end" f08coarray SRC_EXT f90)
+check_fortran_source_compiles("real :: x[*]; end" f08coarray SRC_EXT f90)
 unset(CMAKE_REQUIRED_FLAGS)
 unset(CMAKE_REQUIRED_LIBRARIES)
 
 list(APPEND Coarray_REQUIRED_VARS ${f08coarray})
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(Coarray
-  REQUIRED_VARS Coarray_REQUIRED_VARS)
+find_package_handle_standard_args(Coarray REQUIRED_VARS Coarray_REQUIRED_VARS)
 
-set(Coarray_LIBRARIES ${Coarray_LIBRARY})
+if(Coarray_FOUND)
+  set(Coarray_LIBRARIES ${Coarray_LIBRARY})
+endif()
 
+message(STATUS "Coarray_COMPILE_OPTIONS:" ${Coarray_COMPILE_OPTIONS})
+message(STATUS "Coarray_LIBRARIES:" ${Coarray_LIBRARIES})
+message(STATUS "Coarray_MAX_NUMPROCS:" ${Coarray_MAX_NUMPROCS})
 
-mark_as_advanced(
-  Coarray_LIBRARY
-  Coarray_REQUIRED_VARS
-)
+mark_as_advanced(Coarray_LIBRARY Coarray_REQUIRED_VARS)
--- cmake/SetFortranFlags.cmake
+++ cmake/SetFortranFlags.cmake
@@ -1,11 +1,11 @@
-# ##############################################################################
+# ##################################################################################################
 # Determine and set the Fortran compiler flags we want
-# ##############################################################################
+# ##################################################################################################
 # https://github.com/SethMMorton/cmake_fortran_template
 
-# ##############################################################################
+# ##################################################################################################
 # Make sure that the default build type is RELEASE if not specified.
-# ##############################################################################
+# ##################################################################################################
 include(${CMAKE_MODULE_PATH}/SetCompileFlag.cmake)
 
 # Make sure the build type is uppercase
@@ -16,51 +16,71 @@ message(STATUS "Build type: ${BT}")
 if(BT STREQUAL "RELEASE")
   set(CMAKE_BUILD_TYPE
       RELEASE
-      CACHE STRING
-            "Choose the type of build, options are DEBUG, RELEASE, or TESTING."
-            FORCE)
+      CACHE STRING "Choose the type of build, options are DEBUG or RELEASE" FORCE)
 elseif(BT STREQUAL "DEBUG")
   set(CMAKE_BUILD_TYPE
       DEBUG
-      CACHE STRING
-            "Choose the type of build, options are DEBUG, RELEASE, or TESTING."
-            FORCE)
-elseif(BT STREQUAL "TESTING")
-  set(CMAKE_BUILD_TYPE
-      TESTING
-      CACHE STRING
-            "Choose the type of build, options are DEBUG, RELEASE, or TESTING."
-            FORCE)
+      CACHE STRING "Choose the type of build, options are DEBUG or RELEASE" FORCE)
 elseif(NOT BT)
   set(CMAKE_BUILD_TYPE
       RELEASE
-      CACHE STRING
-            "Choose the type of build, options are DEBUG, RELEASE, or TESTING."
-            FORCE)
+      CACHE STRING "Choose the type of build, options are DEBUG or RELEASE" FORCE)
   message(STATUS "CMAKE_BUILD_TYPE not given, defaulting to RELEASE")
 else()
-  message(
-    FATAL_ERROR
-      "CMAKE_BUILD_TYPE not valid, choices are DEBUG, RELEASE, or TESTING")
+  message(FATAL_ERROR "CMAKE_BUILD_TYPE not valid, choices are DEBUG or RELEASE")
 endif(BT STREQUAL "RELEASE")
 
 # gfortran
 if(CMAKE_Fortran_COMPILER_ID STREQUAL GNU)
-  set(CMAKE_Fortran_FLAGS "-cpp -fconvert=big-endian -ffree-line-length-none -fno-stack-arrays")
-  set(CMAKE_Fortran_FLAGS_DEBUG "-O0 -g -Wall -Wextra -Wpedantic -fimplicit-none -fbacktrace -fcheck=all -fcheck=bounds -ffpe-trap=zero,overflow,underflow -finit-real=nan ")
-  set(CMAKE_Fortran_FLAGS_RELEASE "-O3 -floop-parallelize-all -funroll-loops -finline-functions")
+
+  # There is some bug where -march=native doesn't work on Mac
+  if(APPLE)
+    set(GNUNATIVE "-mtune=native")
+  else()
+    set(GNUNATIVE "-march=native")
+  endif()
+
+  set(CMAKE_Fortran_FLAGS "-cpp -std=f2018 -ffree-line-length-none -fcoarray=lib")
+  set(CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS} ${Coarray_COMPILE_OPTIONS}")
+  set(CMAKE_Fortran_FLAGS_DEBUG
+      "-O0 -g \
+ -Wall -Wextra -Wpedantic -Wconversion \
+ -fimplicit-none -fbacktrace \
+ -fcheck=all -ffpe-trap=zero,overflow,invalid,underflow -finit-real=nan")
+
+  set(CMAKE_Fortran_FLAGS_RELEASE
+      "-O3 -funroll-loops -finline-functions -floop-parallelize-all -ftree-parallelize-loops=6 ${GNUNATIVE}"
+  )
+
+  if(ENABLE_PROFILING)
+    set(CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS} -pg -g")
+  endif()
+
 endif()
 
 # ifort
 if(CMAKE_Fortran_COMPILER_ID STREQUAL Intel)
-  set(CMAKE_Fortran_FLAGS "-fpp -convert big_endian -heap-arrays -fp-model precise -fp-model except -check bounds -std08 -diag-disable 5268 -diag-disable 8770")
-  set(CMAKE_Fortran_FLAGS_DEBUG "-O0 -g -traceback -warn all -debug extended")
-  set(CMAKE_Fortran_FLAGS_RELEASE "-O3 -axCORE-AVX2 -unroll -inline")
-endif()
 
-# There is some bug where -march=native doesn't work on Mac
-if(APPLE)
-  set(GNUNATIVE "-mtune=native")
-else()
-  set(GNUNATIVE "-march=native")
+  # if(SERIAL_BUILD) set(IFORT_COARRAY "-coarray=single") elseif(SHARED_MEMORY) set(IFORT_COARRAY
+  # "-coarray=shared") elseif(DISTRIBUTED_MEMORY) set(IFORT_COARRAY "-coarray=distributed") endif()
+
+  set(IFORT_FLAGS
+      "-fpp -fp-model precise -fp-model except -diag-disable 5268 -diag-disable 8770 ${Coarray_COMPILE_OPTIONS}"
+  )
+
+  # Fortran 2018 standards check based on the version
+  if(CMAKE_Fortran_COMPILER_VERSION VERSION_LESS 19.0.3)
+    set(CMAKE_Fortran_FLAGS "-stand f15 ${IFORT_FLAGS}")
+  else()
+    set(CMAKE_Fortran_FLAGS "-stand f18 ${IFORT_FLAGS}")
+  endif()
+
+  if(ENABLE_PROFILING)
+    set(CMAKE_Fortran_FLAGS
+        "${CMAKE_Fortran_FLAGS} -p -g -qopt-report-phase=all -qopt-report-annotate-position=both -qopt-report=5"
+    )
+  endif()
+
+  set(CMAKE_Fortran_FLAGS_DEBUG "-O0 -g -warn all -debug all -traceback -fpe-all=0 -check bounds")
+  set(CMAKE_Fortran_FLAGS_RELEASE " -O3 -xHost -ipo -parallel -mtune=${TARGET_ARCHITECTURE}")
 endif()
