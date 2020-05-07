ac05737bf86b50a115d88183d16231a99b291989
Miller, Samuel <smil@lle.rochester.edu>
2020-05-07 13:59:48 -0400

    Python scripting updates  - (align with memory refactor branch)

--- pycato/load_datasets.py
+++ pycato/load_datasets.py
@@ -9,7 +9,7 @@ import pint
 import xarray as xr
 
 
-ureg = pint.UnitRegistry()
+from .unit_registry import ureg
 
 
 def read_stepfile(file):
@@ -27,7 +27,15 @@ def read_stepfile(file):
     """
     data = {}
 
-    var_list = ["x", "density", "x_velocity", "y_velocity", "pressure", "sound_speed"]
+    var_list = [
+        "x",
+        "y",
+        "density",
+        "x_velocity",
+        "y_velocity",
+        "pressure",
+        "sound_speed",
+    ]
 
     with h5py.File(file, "r") as h5:
         # Transpose to match the index convention within the code
@@ -95,11 +103,14 @@ def generate_dataset(var_dict, unit_system="icf"):
         space_dims = ("time", "x", "y")
         coords = {
             "time": time.to(time_units).m,
-            "x": x.to(length_units).m,
-            "y": y.to(length_units).m,
+            "x": (["i", "j"], x.to(length_units).m),
+            "y": (["i", "j"], y.to(length_units).m),
         }
     else:
         raise Exception("The dimensionality should be 1D or 2D...")
+
+    print("space_dims", space_dims)
+    print("x", x.shape)
 
     ds = xr.Dataset(
         data_vars={
@@ -238,5 +249,135 @@ def load_1d_dataset(folder, units="cgs"):
         ) * ureg("barye")
 
     ds = generate_dataset(data, unit_system=units)
+
+    return ds
+
+
+def load_2d_dataset(folder, units="cgs"):
+    """Load a 2D CATO dataset into an xarray Dataset container
+
+    Parameters
+    ----------
+    folder : str
+        Folder containing the step files
+    units : str, optional
+        Desired unit system to convert the quantities to, by default 'cgs'. 'icf' will use
+        conventions typical in the ICF community, e.g. [g/cc, um, km/s, eV]
+
+    Returns
+    -------
+    xr.Dataset
+        An xarray Dataset containing the time-varying data
+    """
+
+    if units != "cgs" and units != "icf":
+        raise Exception("Unsupported units not in the set = {cgs, icf}")
+
+    step_files = sorted(
+        [
+            os.path.join(folder, f)
+            for f in os.listdir(folder)
+            if f.endswith(".h5") and f.startswith("step")
+        ]
+    )
+    data = read_stepfile(step_files[0])
+    cell_dims = data["density"].shape
+    nx_cells = cell_dims[0]
+    ny_cells = cell_dims[0]
+    nx_nodes = nx_cells + 1
+    ny_nodes = ny_cells + 1
+    data_dim_t = len(step_files)
+
+    data = {
+        "name": str(folder).split("/")[-1],
+        "time": np.zeros(data_dim_t),
+        "x": np.zeros((data_dim_t, nx_nodes, ny_nodes)),
+        "y": np.zeros((data_dim_t, nx_nodes, ny_nodes)),
+        "density": np.zeros((data_dim_t, nx_cells, ny_cells)),
+        "pressure": np.zeros((data_dim_t, nx_cells, ny_cells)),
+        "sound_speed": np.zeros((data_dim_t, nx_cells, ny_cells)),
+        "x_velocity": np.zeros((data_dim_t, nx_cells, ny_cells)),
+        "y_velocity": np.zeros((data_dim_t, nx_cells, ny_cells)),
+    }
+
+    for t, f in enumerate(step_files):
+        single_step_data = read_stepfile(f)
+        data["x"][t, :] = single_step_data["x"]
+        data["y"][t, :] = single_step_data["y"]
+        data["time"][t] = single_step_data["time"]
+        data["x_velocity"][t, :] = single_step_data["x_velocity"]
+        data["y_velocity"][t, :] = single_step_data["y_velocity"]
+        data["density"][t, :] = single_step_data["density"]
+        data["sound_speed"][t, :] = single_step_data["sound_speed"]
+        data["pressure"][t, :] = single_step_data["pressure"]
+
+    data["density"] = data["density"] * ureg(single_step_data["density_units"])
+    data["pressure"] = data["pressure"] * ureg(single_step_data["pressure_units"])
+    data["sound_speed"] = data["sound_speed"] * ureg(
+        single_step_data["sound_speed_units"]
+    )
+    data["x_velocity"] = data["x_velocity"] * ureg(single_step_data["x_velocity_units"])
+    data["y_velocity"] = data["y_velocity"] * ureg(single_step_data["y_velocity_units"])
+    data["time"] = data["time"] * ureg(single_step_data["time_units"])
+
+    # cell spacing
+    dy = (np.diff(data["x"][0, 0, :]) / 2.0)[0]
+    dx = (np.diff(data["y"][0, :, 0]) / 2.0)[0]
+
+    # cell center locations
+    data["x"] = (data["x"][0, :-1, :-1] + dx) * ureg(single_step_data["x_units"])
+    data["y"] = (data["y"][0, :-1, :-1] + dy) * ureg(single_step_data["x_units"])
+
+    if units == "icf":
+        density_units = "g/cc"
+        pressure_units = "Mbar"
+        vel_units = "um/ns"
+        time_units = "ns"
+        length_units = "um"
+    else:
+        density_units = "g/cc"
+        pressure_units = "barye"
+        vel_units = "cm/s"
+        time_units = "s"
+        length_units = "cm"
+
+    space_dims = ("t", "i", "j")
+    coords = {
+        "time": ("t", data["time"].m),
+        "x": (["i", "j"], data["x"].m),
+        "y": (["i", "j"], data["y"].m),
+    }
+
+    ds = xr.Dataset(
+        data_vars={
+            "density": xr.Variable(
+                space_dims,
+                data["density"].to(density_units).m,
+                attrs={"units": density_units},
+            ),
+            "x_velocity": xr.Variable(
+                space_dims,
+                data["x_velocity"].to(vel_units).m,
+                attrs={"units": vel_units},
+            ),
+            "y_velocity": xr.Variable(
+                space_dims,
+                data["y_velocity"].to(vel_units).m,
+                attrs={"units": vel_units},
+            ),
+            "sound_speed": xr.Variable(
+                space_dims,
+                data["sound_speed"].to(vel_units).m,
+                attrs={"units": vel_units},
+            ),
+            "pressure": xr.Variable(
+                space_dims,
+                data["pressure"].to(pressure_units).m,
+                attrs={"units": pressure_units},
+            ),
+        },
+        coords=coords,
+        attrs={"name": data["name"], "time_units": time_units},
+    )
 
     return ds
