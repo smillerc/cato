96daf47d567b3720cf218b5ce56e266969051d68
Miller, Samuel <smil@lle.rochester.edu>
2020-06-04 12:51:32 -0400

    Somehow the sedov test missed a few in the last merge

--- tests/integrated/sedov/profile.sh
+++
@@ -1,9 +0,0 @@
-source /software/intel/advisor/advixe-vars.sh intel64
-
-opts="--project-dir=./advi --search-dir all:=../src/ -- ./cato.x input.ini"
-
-# Collect the data
-advixe-cl --collect=survey ${opts}
-
-# Save the results
-advixe-cl --report=survey --project-dir=./advi --format=text --report-output=./out/survey.txt
